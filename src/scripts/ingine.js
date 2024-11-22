62d33f4fd409050c5f451ad2cc76bf2101aeb905
Cristopher Andrade <133789100+cristopheroan@users.noreply.github.com>
2024-11-22 17:10:29 +0000

    butão play

--- src/scripts/ingine.js
+++ src/scripts/ingine.js
@@ -5,29 +5,33 @@ const state = {
         timeLeft: document.querySelector("#time-left"),
         score: document.querySelector("#score"),
         life: document.querySelector("#life"),
+        menu: document.getElementById("menu"),
+        game: document.getElementById("game"),
+        playButton: document.getElementById("play-button"),
     },
 
     values:{
         gameVelocity: 1000,
         hitPosition: 0,
         result: 0,
-        curretTime: 60,
+        currentTime: 60,
         life: 4,
     },
     actions: {
-        timerId: setInterval(randomSquare, 1000),
-        countDownTimerId: setInterval(countDown, 1000),
+        timerId: null,
+        countDownTimerId: null,
     },
 };
 
 function countDown() {
-    state.values.curretTime--;
-    state.view.timeLeft.textContent = state.values.curretTime;
+    state.values.currentTime--;
+    state.view.timeLeft.textContent = state.values.currentTime;
 
-    if (state.values.curretTime <= 0) {
-        clearInterval(state.actions.countDownTimerId.countDownTimerId);
+    if (state.values.currentTime <= 0) {
+        clearInterval(state.actions.countDownTimerId);
         clearInterval(state.actions.timerId);
         alert("Game Over! O seu resultado foi: " + state.values.result);
+        showMenu();
     }
 }
 
@@ -74,13 +78,33 @@ function decreaseLife() {
         alert("Game Over! Você perdeu toda a sua vida.");
         clearInterval(state.actions.timerId);
         clearInterval(state.actions.countDownTimerId);
+        showMenu();
     }
 }
 
+function showMenu() {
+    state.view.menu.style.display = "block";
+    state.view.game.style.display = "none";
+}
+
+function startGame() {
+    state.values.result = 0;
+    state.values.currentTime = 60;
+    state.values.life = 3;
+    state.view.score.textContent = state.values.result;
+    state.view.timeLeft.textContent = state.values.currentTime;
+    state.view.life.textContent = `x${state.values.life}`;
+    state.view.menu.style.display = "none";
+    state.view.game.style.display = "block";
+   moveEnemy();
+    state.actions.countDownTimerId = setInterval(countDown, 1000);
+}
+
 function initialize() {
-    moveEnemy();
+    state.view.playButton.addEventListener("click", startGame);
     addListenerHitbox();
-    decreaseLife();
+    showMenu();
+
 }
 
 initialize();
